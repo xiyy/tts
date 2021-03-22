a0a5ee18622e1637b5151d4d6dc29a41921e1405
zhangxiaobei <zhangxiaobei@sogou-inc.com>
2021-03-22 14:39:43 +0800

    添加workFragment和MineFragment

--- app/src/main/java/com/xi/liuliu/tts/view/MineFragment.java
+++ app/src/main/java/com/xi/liuliu/tts/view/MineFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 import com.xi.liuliu.tts.R;
 
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
+    private LinearLayout mLoginLl;
+    private LinearLayout mBillLl;
+    private LinearLayout mHelpLl;
+    private LinearLayout mSettingLl;
+    private LinearLayout mAboutLl;
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -20,6 +26,37 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigator_mine, container, false);
+        mLoginLl = view.findViewById(R.id.mf_ll_login);
+        mLoginLl.setOnClickListener(this);
+        mBillLl = view.findViewById(R.id.mf_spend_bill_ll);
+        mBillLl.setOnClickListener(this);
+        mHelpLl = view.findViewById(R.id.mf_help_ll);
+        mHelpLl.setOnClickListener(this);
+        mSettingLl = view.findViewById(R.id.mf_settings_ll);
+        mSettingLl.setOnClickListener(this);
+        mAboutLl = view.findViewById(R.id.mf_about_ll);
+        mAboutLl.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mf_ll_login:
+
+                break;
+            case R.id.mf_spend_bill_ll:
+
+                break;
+            case R.id.mf_help_ll:
+
+                break;
+            case R.id.mf_settings_ll:
+
+                break;
+            case R.id.mf_about_ll:
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/xi/liuliu/tts/view/WorkFragment.java
+++ app/src/main/java/com/xi/liuliu/tts/view/WorkFragment.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 import com.xi.liuliu.tts.R;
 
-public class WorkFragment extends Fragment {
-
+public class WorkFragment extends Fragment implements View.OnClickListener {
+    private ImageView mGuideToRecordIv;
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -21,6 +22,18 @@ public class WorkFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigator_work, container, false);
+        mGuideToRecordIv = view.findViewById(R.id.wf_iv_no_record);
+        mGuideToRecordIv.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.wf_iv_no_record:
+
+
+                break;
+        }
     }
 }
